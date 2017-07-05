29561db9d219b1020c8db1a806e30d596d26e0db
Sebastian Musiał <kontakt@sebastianmusial.pl>
2017-07-05 23:03:21 +0200

    Presentation

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBSv4do08InNtsMGIMGwb5rdP30I3IA2IU",
-    authDomain: "react-tutorial-e64c1.firebaseapp.com",
-    databaseURL: "https://react-tutorial-e64c1.firebaseio.com",
-    projectId: "react-tutorial-e64c1",
-    storageBucket: "react-tutorial-e64c1.appspot.com",
-    messagingSenderId: "289141873099"
-}
